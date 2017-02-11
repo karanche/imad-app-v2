10abd42f95adffb8b69b73d23e312c9463665f67
karan <bluefoxche@gmail.com>
2017-02-11 22:16:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res){
-    res.send('Article one will send here and will be appear soon');
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+
 });
 
 app.get('/article-two', function (req, res){
