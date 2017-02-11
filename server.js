b209a0f8cf33ac8fcdd4521d79b1130d20335078
karan <bluefoxche@gmail.com>
2017-02-11 22:24:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,11 +16,12 @@ app.get('/article-one', function (req, res){
 });
 
 app.get('/article-two', function (req, res){
-    res.send('Article two will send here and will be appear soon');
+          res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+
 });
 
 app.get('/article-three', function (req, res){
-    res.send('Article three will send here and will be appear soon');
+      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
